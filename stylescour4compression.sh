76dd59561f3415ab11d5630410adf8dd3b790bfd
Johannes Kalliauer <jo.spam89@gmx.at>
2019-03-11 23:35:31 +0100

    2019-03-11

--- stylescour4compression.sh
+++ stylescour4compression.sh
@@ -20,7 +20,7 @@ if [ $meta == 1 ]; then
  echo keep metadata
  export META= 
 elif [ $meta == 0 ]; then
- export META="--remove-metadata --remove-descriptive-elements"
+ export META="--remove-metadata" # --remove-descriptive-elements
  echo delete META=$META
 else
  echo imput not allowed meta is $meta
@@ -36,31 +36,36 @@ fi
 
 echo scour ${file} to $i begin, min=${minfilesize}, meta=$meta, META= $META, INDENT=$INDENT
 
-scour -i ${file} -o $i --keep-unreferenced-defs --enable-comment-stripping --remove-titles --remove-descriptions --set-precision=5 $META $INDENT --renderer-workaround --disable-style-to-xml  --set-c-precision=5  --disable-simplify-colors #--enable-viewboxing #  
+scour -i ${file} -o $i --keep-unreferenced-defs --remove-descriptions --strip-xml-space  --set-precision=6 $META $INDENT --renderer-workaround  --set-c-precision=6 --protect-ids-noninkscape  --disable-simplify-colors  --keep-editor-data # --enable-comment-stripping --create-groups  #--enable-viewboxing #  
 
-#--keep-unreferenced-defs
-## if referenced-defs are deleted: https://github.com/scour-project/scour/issues/155 --> https://github.com/scour-project/scour/issues/174
+# == min == 
+# not --enable-comment-stripping # keep comments
+# not --remove-titles
+# not  --enable-id-stripping
+#no --shorten-ids 
+
+### === CSS ===
+# --keep-unreferenced-defs # https://github.com/scour-project/scour/issues/155
+# --disable-style-to-xml # https://github.com/scour-project/scour/issues/176
+# --disable-simplify-colors # https://github.com/scour-project/scour/issues/221
+# not #--enable-id-stripping # https://github.com/scour-project/scour/issues/164
+# not #--shorten-ids # https://github.com/scour-project/scour/issues/164
+
+## == Bug ==
 
 #--enable-viewboxing
 ##Changes size of view
 ##can create Hairline-cracks f.e in https://commons.wikimedia.org/wiki/File:Mn_coa_zavkhan_aimag.svg
 
-## == Bugs ==
-#--disable-style-to-xml #https://github.com/scour-project/scour/issues/176 #https://github.com/scour-project/scour/issues/174
-#--disable-simplify-colors #https://github.com/scour-project/scour/issues/221
-# --create-groups # Problems related to https://github.com/RazrFalcon/svgcleaner/issues/182 and https://bugs.launchpad.net/inkscape/+bug/1733651
+#--create-groups #https://commons.wikimedia.org/wiki/File:CIA_WorldFactBook-Political_world.svg
 
-# keep ID-Names: no #--shorten-ids and no # --enable-id-stripping
-#--shorten-ids # https://github.com/scour-project/scour/issues/164
-# --enable-id-stripping #https://github.com/scour-project/scour/issues/164
+# --create-groups # https://github.com/scour-project/scour/issues/196 (but it is 
 
-# --create-groups # https://github.com/scour-project/scour/issues/196 (but it Seems to be a chrome and a firefox-bug not a scour-bug
-
+# --set-precision=6 #https://github.com/scour-project/scour/issues/219
 #--set-precision=5 # https://commons.wikimedia.org/wiki/File:Porr_logo.svg
-#--set-c-precision=3 # https://commons.wikimedia.org/wiki/File:LageplanStrasse.svg https://commons.wikimedia.org/wiki/File:Dojikko2.3.svg
 #--set-c-precision=4 # https://commons.wikimedia.org/wiki/File:Flower_soft.svg
 #--set-c-precision=5 #https://commons.wikimedia.org/wiki/File:Anatomy_of_Human_Ear_with_Cochlear_Frequency_Mapping.svg https://commons.wikimedia.org/wiki/File:MichelinStar.svg
-#--set-c-precision=6 same precision as --set-precision=6 #https://commons.wikimedia.org/wiki/File:MichelinStar.svg 
+#--set-c-precision=6 same precision as --set-precision=6 # https://commons.wikimedia.org/wiki/File:MichelinStar.svg
 
 #echo mv ./${file} ./${tmp}3.xml
 mv ./${file} ./${tmp}3.xml
