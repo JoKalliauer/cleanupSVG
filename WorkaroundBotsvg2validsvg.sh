adabf6baefdbdd0131d7a0c5755643c5a8b53b0c
Johannes Kalliauer <jo.spam89@gmx.at>
2019-04-02 07:57:19 +0200

    Update WorkaroundBotsvg2validsvg.sh

--- WorkaroundBotsvg2validsvg.sh
+++ WorkaroundBotsvg2validsvg.sh
@@ -3,15 +3,19 @@
 #Author: Johannes Kalliauer (JoKalliauer)
 #created: 2019-02-20
 
-T35245tspan=YES
-EinzeilTags=YES
-SVGCleaner=NO
+
 
 rm -f $1
 
-wget -q https://commons.wikimedia.org/wiki/Special:FilePath/$1
-
 export i=$1
+
+#----
+
+T35245tspan=YES
+EinzeilTags=YES
+SVGCleaner=YES
+
+wget -q https://commons.wikimedia.org/wiki/Special:FilePath/$i -O $i
 
 if [ $SVGCleaner = 'YES' ]; then
  /data/project/svgworkaroundbot/prgm/svgcleaner/svgcleaner $i tmp.svg --allow-bigger-file --indent 1 --resolve-use no --apply-transform-to-gradients yes --apply-transform-to-shapes yes --convert-shapes yes --group-by-style no --join-arcto-flags no --join-style-attributes no --merge-gradients yes --regroup-gradient-stops yes --remove-comments no --remove-declarations no --remove-default-attributes yes --remove-desc yes --remove-dupl-cmd-in-paths yes --remove-dupl-fegaussianblur yes --remove-dupl-lineargradient yes --remove-dupl-radialgradient yes --remove-gradient-attributes yes --remove-invalid-stops yes --remove-invisible-elements no --remove-metadata no --remove-needless-attributes yes --remove-nonsvg-attributes no --remove-nonsvg-elements no --remove-text-attributes no --remove-title no --remove-unreferenced-ids no --remove-unresolved-classes yes --remove-unused-coordinates yes --remove-unused-defs yes --remove-version yes --remove-xmlns-xlink-attribute yes --simplify-transforms yes --trim-colors yes --trim-ids no --trim-paths yes --ungroup-defs yes --ungroup-groups no --use-implicit-cmds yes --list-separator comma --paths-to-relative yes --remove-unused-segments yes --convert-segments yes --apply-transform-to-paths no --coordinates-precision 2 --paths-coordinates-precision 5 --properties-precision 3 --transforms-precision 7 --copy-on-error
@@ -89,5 +93,5 @@ cp $i $2
 
 python /data/project/shared/pywikipedia/core/scripts/upload.py $i -keep -ignorewarn -noverify -descfile WorkaroundBotsvg2validsvg.sh
 
-rm $i
+#rm $i
 
