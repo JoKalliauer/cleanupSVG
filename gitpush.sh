2b8d02be6b0f50c6252981095cbb6e7e1e448d90
Johannes Kalliauer <jo.spam89@gmx.at>
2019-10-17 11:37:38 +0200

    #!/bin/sh

--- gitpush.sh
+++ gitpush.sh
@@ -1,11 +1,10 @@
+#!/bin/sh
+git config --global core.eol lf
+git config core.eol lf
 git status
-
 git add .
-
 git status
-
 read -p "Press enter to continue" #Press Control C for stop
-
 git status
 git commit -m "Update"
 git status
