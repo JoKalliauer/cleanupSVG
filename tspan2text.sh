66a8745fea91104d3e3d26f5eb9b066d58468e05
Johannes Kalliauer <johannes.kalliauer@tuwien.ac.at>
2022-02-23 09:31:53 +0100

    Update

--- tspan2text.sh
+++ tspan2text.sh
@@ -64,8 +64,8 @@ sed -ri "s/<g([-[:alnum:]\(\)\. ,;:=\"#]*) font-size=\"([[:digit:]\.]*)\"([-[:al
 #  fi
 
 #tspan in tspan
-sed -ri -e ':a' -e 'N' -e '$!ba' -e "s/<tspan([-[:alnum:]\,\.\"\=\:\ #]*) x=\"([-[:digit:]\.\ ]+)\" y=\"([-[:digit:]\.\ ]+)\"([-[:alnum:]\,\.\"\=\:\ #]*)>[[:space:]]*<tspan([-[:alnum:]\,\.\"\=\:\ #]*) x=\"([-[:digit:]\.\ ]+)\" y=\"([-[:digit:]\.\ ]+)\"([-[:alnum:]\,\.\"\=\:\ #\(\)]*)>([- [:alnum:]\.\,\(\)]*)<\/tspan>/<tspan x=\"\6\" y=\"\7\"\1\4\5\8>\9/g" $i
-sed -ri -e ':a' -e 'N' -e '$!ba' -e "s/<tspan([-[:alnum:]\,\.\"\=\:\ #]*) x=\"([-[:digit:]\.\ ]+)\" y=\"([-[:digit:]\.\ ]+)\"([-[:alnum:]\,\.\"\=\:\ #]*)>[[:space:]]*<tspan([-[:alnum:]\,\.\"\=\:\ #\(\)]*)>([- [:alnum:]\.\,\(\)\ä\ü\…]*)<\/tspan>/<tspan x=\"\2\" y=\"\3\"\1\4\5>\6/g" $i
+sed -ri -e ':a' -e 'N' -e '$!ba' -e "s/<tspan([-[:alnum:]\,\.\"\=\:\ #]*) x=\"([-[:digit:]\.\ ]+)\" y=\"([-[:digit:]\.\ ]+)\"([-[:alnum:]\,\.\"\=\:\ #]*)>[[:space:]]*<tspan([-[:alnum:]\,\.\"\=\:\ #]*) x=\"([-[:digit:]\.\ ]+)\" y=\"([-[:digit:]\.\ ]+)\"([-[:alnum:]\,\.\"\=\:\ #\(\)]*)>([- [:alnum:]\.\,\(\)\/@]*)<\/tspan>/<tspan x=\"\6\" y=\"\7\"\1\4\5\8>\9/g" $i #with x,y
+sed -ri -e ':a' -e 'N' -e '$!ba' -e "s/<tspan([-[:alnum:]\,\.\"\=\:\ #]*) x=\"([-[:digit:]\.\ ]+)\" y=\"([-[:digit:]\.\ ]+)\"([-[:alnum:]\,\.\"\=\:\ #]*)>[[:space:]]*<tspan([-[:alnum:]\,\.\"\=\:\ #\(\)]*)>([- [:alnum:]\.\,\(\)\ä\ü\…\/@]*)<\/tspan>/<tspan x=\"\2\" y=\"\3\"\1\4\5>\6/g" $i #without x,y
 #tspan to text
 
 #tspan to text
