e0ab4e11dad87a195b267e769281a2b2e6fd792c
Johannes Kalliauer <johannes.kalliauer@tuwien.ac.at>
2022-08-02 11:57:23 -0400

    Update

--- Rounding.sh
+++ Rounding.sh
@@ -26,6 +26,7 @@ echo $i start:
 
 
  sed -ri "s/ transform=\"scale\((0|).999(8|9)[[:digit:]]*( |,)1.000(0|1|2)[[:digit:]]*\)\"/ transform=\"scale\(1,1.000\)\"/g" $i
+# sed -ri "s/ transform=\"scale\((0|).999(7|8|9)[[:digit:]]*( |,)1.000(0|1|2)[[:digit:]]*\)\"/ transform=\"scale\(1,1.000\)\"/g" $i
 # sed -ri "s/ transform=\"scale\((0|).99[[:digit:]]*( |,)1.0(0|1)[[:digit:]]*\)\"/ transform=\"scale\(1,1.000\)\"/g" $i # more aggressive, replaces text
  sed -ri "s/ transform=\"scale\(1.000(0|1|2)[[:digit:]]*( |,)(0|).999(8|9)[[:digit:]]*\)\"/ transform=\"scale\(1.000,1\)\"/g" $i
  sed -ri "s/ transform=\"scale\(1.000(0|1|2|3)[[:digit:]]*( |,)(0|).999(6|7|8|9)[[:digit:]]*\)\"/ transform=\"scale\(1.000,1\)\"/g" $i # more aggressive, replaces text
