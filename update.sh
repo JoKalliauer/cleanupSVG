b30dd54f3048ab3747437182d6655a258f35217b
Johannes Kalliauer <jo.spam89@gmx.at>
2022-12-20 15:32:33 -0500

    Update

---
+++ update.sh
@@ -0,0 +1,17 @@
+#python -m pip install --upgrade pip
+#sudo python3 -m pip install --upgrade pip
+python3 -m pip install --upgrade pip # --user
+#sudo pip3 install --upgrade https://github.com/codedread/scour/archive/master.zip
+pip3 install --upgrade https://github.com/codedread/scour/archive/master.zip # --user
+rustup update
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+cargo install svgcleaner --force --git https://github.com/RazrFalcon/svgcleaner
+cargo install usvg
+cargo install resvg
+sudo npm i -g npm
+npm i -g npm
+#sudo npm install -g svgo
+#npm i svgo
+npm -g install svgo
+#npm -g install --force --git https://github.com/svg/svgo --upgrade
+#svgo --version
