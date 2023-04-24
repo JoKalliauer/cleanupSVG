e8406dc9e81a477de412e61ba3408bb412bcfea5
Johannes Kalliauer <johannes.kalliauer@tuwien.ac.at>
2023-04-24 16:01:27 -0400

    Update

--- tspan2text.sh
+++ tspan2text.sh
@@ -64,13 +64,14 @@ sed -ri "s/<g([-[:alnum:]\(\)\. ,;:=\"#]*) font-size=\"([[:digit:]\.]*)\"([-[:al
 #  fi
 
 #tspan in tspan
-sed -ri -e ':a' -e 'N' -e '$!ba' -e "s/<tspan([-[:alnum:]\,\.\"\=\:\ #]*) x=\"([-[:digit:]\.\ ]+)\" y=\"([-[:digit:]\.\ ]+)\"([-[:alnum:]\,\.\"\=\:\ #]*)>[[:space:]]*<tspan([-[:alnum:]\,\.\"\=\:\ #]*) x=\"([-[:digit:]\.\ ]+)\" y=\"([-[:digit:]\.\ ]+)\"([-[:alnum:]\,\.\"\=\:\ #\(\)]*)>([- [:alnum:]\.\,\(\)\/@]*)<\/tspan>/<tspan x=\"\6\" y=\"\7\"\1\4\5\8>\9/g" $i #with x,y
-sed -ri -e ':a' -e 'N' -e '$!ba' -e "s/<tspan([-[:alnum:]\,\.\"\=\:\ #]*) x=\"([-[:digit:]\.\ ]+)\" y=\"([-[:digit:]\.\ ]+)\"([-[:alnum:]\,\.\"\=\:\ #]*)>[[:space:]]*<tspan([-[:alnum:]\,\.\"\=\:\ #\(\)]*)>([- [:alnum:]\.\,\(\)\ä\ü\…\/@]*)<\/tspan>/<tspan x=\"\2\" y=\"\3\"\1\4\5>\6/g" $i #without x,y
+sed -ri -e ':a' -e 'N' -e '$!ba' -e "s/<tspan([-[:alnum:]\,\.\"\=\:\ #]*) x=\"([-[:digit:]\.\ ]+)\" y=\"([-[:digit:]\.\ ]+)\"([-[:alnum:]\,\.\"\=\:\ #]*)>[[:space:]]*<tspan([-[:alnum:]\,\.\"\=\:\ #]*) x=\"([-[:digit:]\.\ ]+)\" y=\"([-[:digit:]\.\ ]+)\"([-[:alnum:]\,\.\"\=\:\ #\(\)]*)>([- [:alnum:]\.\,\(\)\/@%]*)<\/tspan>/<tspan x=\"\6\" y=\"\7\"\1\4\5\8>\9/g" $i #with x,y
+#<text x="25.44" y="89.7"><tspan x="25.44" y="89.7"><tspan>5 %</tspan></tspan></text>
+sed -ri -e ':a' -e 'N' -e '$!ba' -e "s/<tspan([-[:alnum:]\,\.\"\=\:\ #]*) x=\"([-[:digit:]\.\ ]+)\" y=\"([-[:digit:]\.\ ]+)\"([-[:alnum:]\,\.\"\=\:\ #]*)>[[:space:]]*<tspan([-[:alnum:]\,\.\"\=\:\ #\(\)]*)>([- [:alnum:]\.\,\(\)\ä\ü\…\/@% ]*)<\/tspan>/<tspan x=\"\2\" y=\"\3\"\1\4\5>\6/g" $i #without x,y
 #tspan to text
 
 #tspan to text
 sed -ri  -e ':a' -e 'N' -e '$!ba' -e "s/<text([-[:alnum:]\,\.\"\=\:\ #\(\)]*)( x=\"[-[:digit:]\.\ ]+\" y=\"[-[:digit:]\.\ ]+\")/<text\2\1/g" $i # that x ist at the beginning (otherwise the next line would not work)
-sed -ri  -e ':a' -e 'N' -e '$!ba' -e "s/<text( x=\"[-[:digit:]\.\ ]+\" y=\"[-[:digit:]\.\ ]+\"|)([-[:alnum:]\,\.\"\=\:\ \#\(\)\%\']*)>[[:space:]]*<tspan([-[:alnum:]\,\.\"\=\:\ \#\'\(\)]*) x=\"([-[:digit:]\.\ ]+)\" y=\"([-[:digit:]\.\ ]+)\"([-[:alnum:]\,\.\"\=\:\ #]*)>([-–[:alnum:]\.\ \,\'\♭\♯\/%≈¹²³⁴⁶⁷⁸⁹\^•…\{\}\(\)\+\ä\ü\á\…°_$=:*≤±×]*)<\/tspan>/<text x=\"\4\" y=\"\5\"\2\3\6>\7/g" $i #removes the first tspan of a text element
+sed -ri  -e ':a' -e 'N' -e '$!ba' -e "s/<text( x=\"[-[:digit:]\.\ ]+\" y=\"[-[:digit:]\.\ ]+\"|)([-[:alnum:]\,\.\"\=\:\ \#\(\)\%\']*)>[[:space:]]*<tspan([-[:alnum:]\,\.\"\=\:\ \#\'\(\)]*) x=\"([-[:digit:]\.\ ]+)\" y=\"([-[:digit:]\.\ ]+)\"([-[:alnum:]\,\.\"\=\:\ #]*)>([-–[:alnum:]\.\ \,\'\♭\♯\/%≈¹²³⁴⁶⁷⁸⁹\^•…\{\}\(\)\+\ä\ü\á\…°_$=:*≤±× %]*)<\/tspan>/<text x=\"\4\" y=\"\5\"\2\3\6>\7/g" $i #removes the first tspan of a text element
 
 
 
